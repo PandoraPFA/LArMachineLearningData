d72f0d0e054702d66e6f552b86bbb6d2578738e1
a-d-smith <andrewsmithweb@gmail.com>
2018-02-23 17:03:15 +0000

    Renamed SVM file

---
+++ download.sh
@@ -0,0 +1,11 @@
+function download() {
+   wget --no-check-certificate --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=$1' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=$1" -O $2 && rm -rf /tmp/cookies.txt
+}
+
+# Google Drive links have the form https://drive.google.com/file/d/FILEID/view?usp=sharing
+# You want the FILEID
+# Note: For some reason the above function doesn't work for the files, so you need to set the two &id=FILEID instances in the link
+
+# PandoraMVAData
+wget --no-check-certificate --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1b3m9Glj1Qjx5tnSdvqLIBNFBWpH8NrvX' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1b3m9Glj1Qjx5tnSdvqLIBNFBWpH8NrvX" -O PandoraSvm_v03_11_00.xml && rm -rf /tmp/cookies.txt
+
