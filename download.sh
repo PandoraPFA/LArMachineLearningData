8163fe3ff19bee3c798a41c1483783f7675c31ac
Andrew Chappell <A.Chappell@warwick.ac.uk>
2020-09-29 13:18:10 +0100

    Add Python scripts for training image generation, network training and network conversion (LibTorch)

--- download.sh
+++ download.sh
@@ -12,3 +12,8 @@ wget --no-check-certificate --load-cookies /tmp/cookies.txt "https://docs.google
 wget --no-check-certificate --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1BcZk3aBWj_VEiGTMZchtwM73YT5yWEaW' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1BcZk3aBWj_VEiGTMZchtwM73YT5yWEaW" -O PandoraBdt_v03_20_00.xml && rm -rf /tmp/cookies.txt
 wget --no-check-certificate --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1xUnLN-oWg-c5u4docaxTijMVFIq3faga' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1xUnLN-oWg-c5u4docaxTijMVFIq3faga" -O PandoraBdt_ProtoDUNE_v03_20_00.xml && rm -rf /tmp/cookies.txt
 
+# PandoraNetworkData
+download "1J-XIKtbiomPlTwXIdSdVBFYJsCDlr4F8" "PandoraUnet_TSID_DUNEFD_U_v03_22_00.pt"
+download "1EyJkv_VQl5evQ1LugCnPrTauMAqGei7t" "PandoraUnet_TSID_DUNEFD_V_v03_22_00.pt"
+download "1AwZfhcUeDquTtvi8MiXPHV86kLysYlsv" "PandoraUnet_TSID_DUNEFD_W_v03_22_00.pt"
+
