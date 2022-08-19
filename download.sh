01ee33046aa57c486da51f28223ab5c0ffaaecf4
Andrew Chappell <A.Chappell@warwick.ac.uk>
2022-08-19 22:29:49 +0100

    Add DUNEHD vertex finding network

--- download.sh
+++ download.sh
@@ -49,7 +49,13 @@ mkdir -p $MY_TEST_AREA/LArMachineLearningData/PandoraNetworkData
 cd $MY_TEST_AREA/LArMachineLearningData/PandoraNetworkData
 
 # DUNE
-download "1EOmyofKFl9NAZqRh-z34bY7-M9lXvG9U" "PandoraUnet_TSID_DUNEFD_U_v03_25_00.pt"
-download "1NqeKHnCkWdTIKw6CLqUZp4l-q7EIkAQa" "PandoraUnet_TSID_DUNEFD_V_v03_25_00.pt"
-download "1kWfT8d4GtlMsoxh6cH0quIUNRdHSClhj" "PandoraUnet_TSID_DUNEFD_W_v03_25_00.pt"
+#download "1EOmyofKFl9NAZqRh-z34bY7-M9lXvG9U" "PandoraUnet_TSID_DUNEFD_U_v03_25_00.pt"
+#download "1NqeKHnCkWdTIKw6CLqUZp4l-q7EIkAQa" "PandoraUnet_TSID_DUNEFD_V_v03_25_00.pt"
+#download "1kWfT8d4GtlMsoxh6cH0quIUNRdHSClhj" "PandoraUnet_TSID_DUNEFD_W_v03_25_00.pt"
 
+download "10AzTGyaW-XwYz5i-jNUxZXW77dt4SNZH" "PandoraUnet_Vertex_DUNEFD_1_U_v04_00_00.pt"
+download "128iZ6lYFzW6URxHarMdmUG7p8xO_qtlT" "PandoraUnet_Vertex_DUNEFD_1_V_v04_00_00.pt"
+download "1VMBuNyS3U3OLI1RegC5Pxg3xBf8uA_n8" "PandoraUnet_Vertex_DUNEFD_1_W_v04_00_00.pt"
+download "1nFy3exwMA8BeH-x6mWGSf0SwRHrTbS2C" "PandoraUnet_Vertex_DUNEFD_2_U_v04_00_00.pt"
+download "12-fMYzUSXqNqt21IMWnFMXgQ6y1tcuS-" "PandoraUnet_Vertex_DUNEFD_2_V_v04_00_00.pt"
+download "1Z-5ZjInzKcfcgXClt9jMM66ZJEfEP2-Z" "PandoraUnet_Vertex_DUNEFD_2_W_v04_00_00.pt"
