e90bd6d787ab2f3a32165c66bd01f8eb64d8f15c
Andrew Chappell <A.Chappell@warwick.ac.uk>
2025-02-11 10:04:10 +0000

    Split dune workflow downloads

--- download.sh
+++ download.sh
@@ -12,7 +12,7 @@ function curl_download() {
 
 if [ -z $1 ]
 then
-  echo "Error: Expected usage source download.sh <experiment>"
+  echo "Error: Expected usage source download.sh <experiment> <workflow>"
   echo "   where experiment in [ dune sbnd uboone]"
   return 1
 fi
@@ -70,30 +70,43 @@ cd $MY_TEST_AREA/LArMachineLearningData/PandoraNetworkData
 
 if [[ "$1" == "dune" ]]
 then
-  download "1kd2QqW2hivCTlKD2pgVCQifsgie8QwD9" "PandoraNet_Vertex_DUNEFD_HD_Accel_1_U_v04_06_00.pt"
-  download "16_PRz7Flch9rKyv2b3z4pli4WyhUXKqO" "PandoraNet_Vertex_DUNEFD_HD_Accel_1_V_v04_06_00.pt"
-  download "1-_hxLuNO3q59BTIiuiECI2Fq_MMLctEj" "PandoraNet_Vertex_DUNEFD_HD_Accel_1_W_v04_06_00.pt"
-  download "1GSWeLcZZ1xBWB9qRFfWbUeqgBw4oJu7o" "PandoraNet_Vertex_DUNEFD_HD_Accel_2_U_v04_06_00.pt"
-  download "1v4KpSSekvuhQAKV1S32u_yNCCaQQXWYK" "PandoraNet_Vertex_DUNEFD_HD_Accel_2_V_v04_06_00.pt"
-  download "1UGIp1mcIADZujzCW7vxiryanWYF77wjm" "PandoraNet_Vertex_DUNEFD_HD_Accel_2_W_v04_06_00.pt"
+  if [ -z $2 ]
+  then
+    echo "Error: Expected usage source download.sh dune <workflow>"
+    echo "   where workflow in [ lbl atmos lowe ]"
+    return 1
+  fi
 
-  download "15y8Nn66mP8lVO9NP4jZTKNdTTEfJQSrA" "PandoraNet_Vertex_DUNEFD_VD_Accel_1_U_v04_06_00.pt"
-  download "1PakLUma5gJ34hXEEvgSMfV0_kky4_IMB" "PandoraNet_Vertex_DUNEFD_VD_Accel_1_V_v04_06_00.pt"
-  download "1s2DEXlgNkjTNWUyUXIJm5718OMuj3a4p" "PandoraNet_Vertex_DUNEFD_VD_Accel_1_W_v04_06_00.pt"
-  download "1K2zGwPKfPNI0WyArR_RBO3by0rpUijr3" "PandoraNet_Vertex_DUNEFD_VD_Accel_2_U_v04_06_00.pt"
-  download "1RTaWO_ilYdqTd-qpjGBAmOKuMobG0tIM" "PandoraNet_Vertex_DUNEFD_VD_Accel_2_V_v04_06_00.pt"
-  download "1867oCU1ZxPLiRMQE5SlZDswzfhz8rgSM" "PandoraNet_Vertex_DUNEFD_VD_Accel_2_W_v04_06_00.pt"
+  if [[ "$2" == "lbl" ]]
+  then
+    download "1kd2QqW2hivCTlKD2pgVCQifsgie8QwD9" "PandoraNet_Vertex_DUNEFD_HD_Accel_1_U_v04_06_00.pt"
+    download "16_PRz7Flch9rKyv2b3z4pli4WyhUXKqO" "PandoraNet_Vertex_DUNEFD_HD_Accel_1_V_v04_06_00.pt"
+    download "1-_hxLuNO3q59BTIiuiECI2Fq_MMLctEj" "PandoraNet_Vertex_DUNEFD_HD_Accel_1_W_v04_06_00.pt"
+    download "1GSWeLcZZ1xBWB9qRFfWbUeqgBw4oJu7o" "PandoraNet_Vertex_DUNEFD_HD_Accel_2_U_v04_06_00.pt"
+    download "1v4KpSSekvuhQAKV1S32u_yNCCaQQXWYK" "PandoraNet_Vertex_DUNEFD_HD_Accel_2_V_v04_06_00.pt"
+    download "1UGIp1mcIADZujzCW7vxiryanWYF77wjm" "PandoraNet_Vertex_DUNEFD_HD_Accel_2_W_v04_06_00.pt"
 
-  download "1BYX6Y0sirNzBw0qsj6Xsa-qOAdqn5PG7" "PandoraNet_Vertex_DUNEFD_HD_Atmos_1_U_v04_03_00.pt"
-  download "1AWywC9LsCkuNo6lVo5VAWvlM2n-FWTnx" "PandoraNet_Vertex_DUNEFD_HD_Atmos_1_V_v04_03_00.pt"
-  download "1CLV64endpentEDSoRFTu6obWoYL2Bx-a" "PandoraNet_Vertex_DUNEFD_HD_Atmos_1_W_v04_03_00.pt"
-  download "1HYhF3xGcZXOVxXrjDnYgV_34-q5d58UC" "PandoraNet_Vertex_DUNEFD_HD_Atmos_2_U_v04_03_00.pt"
-  download "1DDqVFhnRBPy6ZIehd9u1SwyiKaWC3hSF" "PandoraNet_Vertex_DUNEFD_HD_Atmos_2_V_v04_03_00.pt"
-  download "1JLr5GfjSNt6vO81ZCv42UHH09hjvtFw7" "PandoraNet_Vertex_DUNEFD_HD_Atmos_2_W_v04_03_00.pt"
+    download "15y8Nn66mP8lVO9NP4jZTKNdTTEfJQSrA" "PandoraNet_Vertex_DUNEFD_VD_Accel_1_U_v04_06_00.pt"
+    download "1PakLUma5gJ34hXEEvgSMfV0_kky4_IMB" "PandoraNet_Vertex_DUNEFD_VD_Accel_1_V_v04_06_00.pt"
+    download "1s2DEXlgNkjTNWUyUXIJm5718OMuj3a4p" "PandoraNet_Vertex_DUNEFD_VD_Accel_1_W_v04_06_00.pt"
+    download "1K2zGwPKfPNI0WyArR_RBO3by0rpUijr3" "PandoraNet_Vertex_DUNEFD_VD_Accel_2_U_v04_06_00.pt"
+    download "1RTaWO_ilYdqTd-qpjGBAmOKuMobG0tIM" "PandoraNet_Vertex_DUNEFD_VD_Accel_2_V_v04_06_00.pt"
+    download "1867oCU1ZxPLiRMQE5SlZDswzfhz8rgSM" "PandoraNet_Vertex_DUNEFD_VD_Accel_2_W_v04_06_00.pt"
 
-  download "13oGLdQKE5tFuvh2c0_06xwU8-8qjUQxH" "PandoraNet_SecVertex_DUNEFD_HD_Accel_1_U_v04_13_00.pt"
-  download "1fmDwr9kJ5RNfIl3u3Dq5P9T5mIy3_ZbB" "PandoraNet_SecVertex_DUNEFD_HD_Accel_1_V_v04_13_00.pt"
-  download "1XOLRg-xydjPx-7i7OCsk88dRzpCgrMyN" "PandoraNet_SecVertex_DUNEFD_HD_Accel_1_W_v04_13_00.pt"
+    download "13oGLdQKE5tFuvh2c0_06xwU8-8qjUQxH" "PandoraNet_SecVertex_DUNEFD_HD_Accel_1_U_v04_13_00.pt"
+    download "1fmDwr9kJ5RNfIl3u3Dq5P9T5mIy3_ZbB" "PandoraNet_SecVertex_DUNEFD_HD_Accel_1_V_v04_13_00.pt"
+    download "1XOLRg-xydjPx-7i7OCsk88dRzpCgrMyN" "PandoraNet_SecVertex_DUNEFD_HD_Accel_1_W_v04_13_00.pt"
+  fi
+
+  if [[ "$2" == "atmos" ]]
+  then
+    download "1BYX6Y0sirNzBw0qsj6Xsa-qOAdqn5PG7" "PandoraNet_Vertex_DUNEFD_HD_Atmos_1_U_v04_03_00.pt"
+    download "1AWywC9LsCkuNo6lVo5VAWvlM2n-FWTnx" "PandoraNet_Vertex_DUNEFD_HD_Atmos_1_V_v04_03_00.pt"
+    download "1CLV64endpentEDSoRFTu6obWoYL2Bx-a" "PandoraNet_Vertex_DUNEFD_HD_Atmos_1_W_v04_03_00.pt"
+    download "1HYhF3xGcZXOVxXrjDnYgV_34-q5d58UC" "PandoraNet_Vertex_DUNEFD_HD_Atmos_2_U_v04_03_00.pt"
+    download "1DDqVFhnRBPy6ZIehd9u1SwyiKaWC3hSF" "PandoraNet_Vertex_DUNEFD_HD_Atmos_2_V_v04_03_00.pt"
+    download "1JLr5GfjSNt6vO81ZCv42UHH09hjvtFw7" "PandoraNet_Vertex_DUNEFD_HD_Atmos_2_W_v04_03_00.pt"
+  fi
 fi
 
 # DUNE ND
