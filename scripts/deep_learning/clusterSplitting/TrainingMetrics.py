476557371942b67051c70411ec2e0a8dc0008596
Isobel Mawby <imawby@lahepgpu2024.lancs.ac.uk>
2025-12-03 13:25:47 +0000

    cleaning up code

--- scripts/deep_learning/clusterSplitting/TrainingMetrics.py
+++ scripts/deep_learning/clusterSplitting/TrainingMetrics.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 
 ################################################################################################################################################
+# Encoder
 ################################################################################################################################################
 
 def plot_scores_class(scores_train, scores_test, truth_train, truth_test, score_class_index) :
@@ -11,7 +12,7 @@ def plot_scores_class(scores_train, scores_test, truth_train, truth_test, score_
     class_scores_train = scores_train[:, score_class_index]
     class_scores_test = scores_test[:, score_class_index]
     
-    for class_index in [0,1,2] :
+    for class_index in [0, 1, 2] :
     
         this_scores_train = class_scores_train[truth_train == class_index]
         this_scores_test = class_scores_test[truth_test == class_index]
@@ -28,54 +29,20 @@ def plot_scores_class(scores_train, scores_test, truth_train, truth_test, score_
         plt.hist(this_scores_train, bins=50, range=(0, 1.0), color=graph_color, label=(legend_string + ' train'), weights=plotting_weights_train, histtype='step', linestyle='solid')
         plt.hist(this_scores_test, bins=50, range=(0, 1.0), color=graph_color, label=(legend_string + ' test'), weights=plotting_weights_test, histtype='step', linestyle='dashed')
 
-    
-    #plt.ylim(0, 0.8)
     plt.yscale("log")
-    
     plt.xlabel(('Classification Score For Class: ' + str(score_class_index)))
-    #plt.ylabel('log(Proportion of Showers)')
-    plt.ylabel('Proportion of Clusters')
+    plt.ylabel('log(Proportion of Clusters)')
     plt.legend(loc='best')
-    plt.show()  
-
-
-################################################################################################################################################
-################################################################################################################################################
-
-def plot_scores(true_scores_train, false_scores_train, true_scores_val, false_scores_val) :
-    
-    true_plotting_weights_train = 1.0 / float(true_scores_train.shape[0])
-    true_plotting_weights_train = torch.ones(true_scores_train.shape) * true_plotting_weights_train
-    false_plotting_weights_train = 1.0 / float(false_scores_train.shape[0])
-    false_plotting_weights_train = torch.ones(false_scores_train.shape) * false_plotting_weights_train
-    true_plotting_weights_val = 1.0 / float(true_scores_val.shape[0])
-    true_plotting_weights_val = torch.ones(true_scores_val.shape) * true_plotting_weights_val
-    false_plotting_weights_val = 1.0 / float(false_scores_val.shape[0])
-    false_plotting_weights_val = torch.ones(false_scores_val.shape) * false_plotting_weights_val    
-    
-    plt.hist(true_scores_train, bins=50, range=(0, 1.0), color='blue', label='signal_train', weights=true_plotting_weights_train, histtype='step', linestyle='solid')
-    plt.hist(false_scores_train, bins=50, range=(0, 1.0), color='red', label='background_train', weights=false_plotting_weights_train, histtype='step', linestyle='solid')
-    plt.hist(true_scores_val, bins=50, range=(0, 1.0), color='blue', label='signal_test', weights=true_plotting_weights_val, histtype='step', linestyle='dashed')
-    plt.hist(false_scores_val, bins=50, range=(0, 1.0), color='red', label='background_test', weights=false_plotting_weights_val, histtype='step', linestyle='dashed')    
-
-    
-    #plt.ylim(0, 0.8)
-    #plt.yscale("log")
-    
-    plt.xlabel('Classification Score')
-    #plt.ylabel('log(Proportion of Showers)')
-    plt.ylabel('Proportion of Showers')
-    plt.legend(loc='best')
-    plt.show()  
+    plt.show() 
     
 ################################################################################################################################################
 ################################################################################################################################################      
-
-def draw_confusion(scores, labels, threshold):
+    
+def draw_confusion_class(scores, labels, threshold):
     
     n_classes = scores.shape[1] + 1
     pred = np.argmax(scores, axis=1)
-    pred[scores.max(axis=1) < threshold] = (n_classes - 1)   # optional reject case
+    pred[scores.max(axis=1) < threshold] = (n_classes - 1) # optional reject case
     confMatrix = confusion_matrix(labels, pred)
     
     trueSums = np.sum(confMatrix, axis=1)
@@ -97,24 +64,51 @@ def draw_confusion(scores, labels, threshold):
 
     displayPredNorm = ConfusionMatrixDisplay(confusion_matrix=predNormalised, display_labels=range(n_classes))
     displayPredNorm.plot()     
-    plt.show()    
+    plt.show()        
+
+################################################################################################################################################
+# Encoder-decoder
+################################################################################################################################################
+
+def plot_scores(scores_train, scores_test, truth_train, truth_test) :
+
+    true_scores_train = scores_train[truth_train == 1]
+    false_scores_train = scores_train[truth_train == 0]
+    true_scores_test = scores_test[truth_test == 1]
+    false_scores_test = scores_test[truth_test == 0]
     
+    true_plotting_weights_train = 1.0 / float(true_scores_train.shape[0])
+    true_plotting_weights_train = torch.ones(true_scores_train.shape) * true_plotting_weights_train
+    false_plotting_weights_train = 1.0 / float(false_scores_train.shape[0])
+    false_plotting_weights_train = torch.ones(false_scores_train.shape) * false_plotting_weights_train
+    true_plotting_weights_test = 1.0 / float(true_scores_test.shape[0])
+    true_plotting_weights_test = torch.ones(true_scores_test.shape) * true_plotting_weights_test
+    false_plotting_weights_test = 1.0 / float(false_scores_test.shape[0])
+    false_plotting_weights_test = torch.ones(false_scores_test.shape) * false_plotting_weights_test    
+    
+    plt.hist(true_scores_train, bins=50, range=(0, 1.0), color='blue', label='signal_train', weights=true_plotting_weights_train, histtype='step', linestyle='solid')
+    plt.hist(false_scores_train, bins=50, range=(0, 1.0), color='red', label='background_train', weights=false_plotting_weights_train, histtype='step', linestyle='solid')
+    plt.hist(true_scores_test, bins=50, range=(0, 1.0), color='blue', label='signal_test', weights=true_plotting_weights_test, histtype='step', linestyle='dashed')
+    plt.hist(false_scores_test, bins=50, range=(0, 1.0), color='red', label='background_test', weights=false_plotting_weights_test, histtype='step', linestyle='dashed')    
+    
+    plt.yscale("log")
+    plt.xlabel('Classification Score')
+    plt.ylabel('log(Proportion of Clusters)')
+    plt.legend(loc='best')
+    plt.show()
     
 ################################################################################################################################################
-################################################################################################################################################    
+################################################################################################################################################      
 
-def draw_confusion_with_threshold(scores, labels, threshold):
+def draw_confusion(pred, labels, threshold):
     
     n_classes = 2
-    
+    scores = pred.copy()
     predicted_true_mask = scores > threshold
     predicted_false_mask = np.logical_not(predicted_true_mask)
     scores[predicted_true_mask] = 1
     scores[predicted_false_mask] = 0
-
     confMatrix = confusion_matrix(labels, scores)
-    
-    print(confMatrix)
     
     trueSums = np.sum(confMatrix, axis=1)
     predSums = np.sum(confMatrix, axis=0)
@@ -134,4 +128,9 @@ def draw_confusion_with_threshold(scores, labels, threshold):
     displayTrueNorm.plot()
 
     displayPredNorm = ConfusionMatrixDisplay(confusion_matrix=predNormalised, display_labels=["False", "True"])
-    displayPredNorm.plot()        
+    displayPredNorm.plot()  
+    
+################################################################################################################################################
+################################################################################################################################################    
+
+ 
